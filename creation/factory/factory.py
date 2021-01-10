1390b546fec50f0537733705d1d08627f104d5b6
Kimsehwan96 <sktpghks138@gmail.com>
2021-01-10 21:02:38 +0900

    add staticmethod decorator

--- creation/factory/factory.py
+++ creation/factory/factory.py
@@ -22,6 +22,7 @@ class Firebat(Unit):
         pass
 
 class UnitFactory: #유닛을 분류하는 단일 책임을 지는 팩토리 클래스
+    @staticmethod
     def create(self, unit_name):
         if unit_name == "Marine":
             return Marine()
@@ -32,6 +33,6 @@ class UnitFactory: #유닛을 분류하는 단일 책임을 지는 팩토리 클
 class Map: #이 맵은 이렇게 되면 유닛을 생성한다는 책임 외에 유닛을 분류한다는 팩토리 클래스에서 갖고있는 이런 책임을 안가지니까, 단일책임원칙을 잘 지킨다.
     def __init__(self, map_file:deque):
         while map_file:
-            unit = UnitFactory.create(map_file.popleft())
+            unit = UnitFactory.create(map_file.popleft()) #UnitFactory 인스턴스를 굳인 생성하지 않고 메서드를 사용하기 위해 static method 데코레이터 추가
     
     #이후 기타 코드들
